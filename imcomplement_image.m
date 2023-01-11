42deca2b8515d97ea99ca00ab108ac6de18f7fc1
Yulia-Didun <julija.didun@gmail.com>
2023-01-11 17:06:30 +0200

    files delete

--- imcomplement_image.m
+++
@@ -1,9 +0,0 @@
-image=imread('street.jpg');
-
-image_inc=imcomplement(image);
-
-subplot(1,2,1);
-imshow(image), title('Original');
-
-subplot(1,2,2);
-imshow(image_inc), title('Negative');
--- negative.m
+++
@@ -1,8 +0,0 @@
-image=imread('street.jpg');
-subplot(1,2,1);
-imshow(image), title('Original');
-
-image_negative=256-image;
-
-subplot(1,2,2);
-imshow(image_negative), title('Negative');
